41a006dc98e9443d58346a08130f47d32910352f
Nooa Sarkkinen <nooa.sarkkinen@gmail.com>
2024-02-12 23:08:53 +0200

    Reworked headed. Added nav bar + sliding effect

--- java/java.js
+++ java/java.js
@@ -1,12 +1,31 @@
 function toggle(){
 
-    var body = document.body; 
-    body.id = ( body.id ) ? body.id : 'body_id'; // ffox
+var body = document.body; 
+body.id = ( body.id ) ? body.id : 'body_id'; // ffox
 
-    body.use_custom_cursor = !body.use_custom_cursor;
+body.use_custom_cursor = !body.use_custom_cursor;
 
-    body.style.cursor = body.use_custom_cursor ? 'wait' : 'auto';
+body.style.cursor = body.use_custom_cursor ? 'wait' : 'auto';
 }
+
+// Wait for the DOM to fully load
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the hamburger menu and navigation menu elements
+    var hamburger = document.querySelector('.hamburger-menu');
+    var navMenu = document.getElementById('navbar');
+    var closeButton = document.getElementById('closeMenu'); // Get the close button
+
+    // Listen for a click event on the hamburger menu
+    hamburger.addEventListener('click', function () {
+        // Toggle the 'visible' class on the navigation menu
+        navMenu.classList.toggle('visible');
+    });
+
+    closeButton.addEventListener('click', function() {
+        navMenu.classList.remove('visible'); // Hide the menu
+    });
+    
+});
 
 // function open_nav() {
 //     if (document.getElementById('navigation').style.display == 'none') { 
